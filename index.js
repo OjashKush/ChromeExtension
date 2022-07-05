6b838186f68465bdb6ad246fc2e4b18ec60365d7
Ojash Kushwaha <ojashkush@gmail.com>
2022-07-05 11:06:22 +0530

    stringify

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let myLeads = []
+
+myLeads = JSON.stringify(myLeads)
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -6,13 +9,14 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+     
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     }
     ulEl.innerHTML = listItems  
 }      
