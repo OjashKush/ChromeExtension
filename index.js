a9b9013a40de1c728684e0e327b6a7d72263605d
Ojash Kushwaha <ojashkush@gmail.com>
2022-07-06 13:04:49 +0530

    Adding save tab

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 //declare an array to store links
 let myLeads = []
 
-//take data from localStorage
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
-
 //declare fixed variables 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 //use presaved values, if any
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
+
+//save tab button
+tabBtn.addEventListener("click", function(){
+    //api to get current link 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+    
+    
+})
 
 //save link, remove input text, save to localStorage, call renderLeads
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) ) 
-    renderLeads()
+    render(myLeads)
 })
 
-//show links below
-function renderLeads() {
+//making DOM
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+         <li>
+              <a target='_blank' href='${leads[i]}'> 
+                     ${leads[i]} 
+              </a>
+        </li>
+        `  
     }
     ulEl.innerHTML = listItems  
 }  
@@ -38,5 +55,5 @@ function renderLeads() {
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
